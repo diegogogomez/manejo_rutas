24bef6725aabca7ad5ff5db28dc33f9be6d35e29
Diego Gomez <d.fernandogomez94@gmail.com>
2020-09-04 22:27:37 -0500

    Commit 2, subida directamente desde visual studio

---
+++ src/app/messages/message.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MessageService } from './message.service';
+import { Message } from './message.model';
+import { Observable } from 'rxjs';
+
+@Component({
+    selector: 'paMessages',
+    templateUrl: 'message.component.html',
+})
+export class MessageComponent {
+    lastMessage: Message;
+    constructor(messageService: MessageService) {
+        messageService.messages.subscribe(m => this.lastMessage = m);
+    }
+    /*
+    constructor(messageService: MessageService) {
+        messageService.messages.subscribe(m => this.lastMessage = m);
+    }*/
+}
